5ee38a7e64f30b22751a5f1fef300f2565d3ad72
Egor4ek-debug <nabatov.egor@gmail.com>
2022-11-27 01:46:36 +0300

    fitst commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+        <ul className='left_menu'>
+              <li><a href="#"><img src={require("./components/Logo.png")} alt="" /></a></li>
+              <li><a href="#"><img src={require("./components/Pages.png")} alt="" /></a></li>
+              <li><a href="#"><img className='Sptites' src={require("./components/Facebook.png")} alt="" /></a></li>
+              <li><a href="#"><img className='Sptites' src={require("./components/Twitter.png")} alt="" /></a></li>
+              <li><a href="#"><img className='Sptites' src={require("./components/Instagram.png")} alt="" /></a></li>
+        </ul>
+        <ul className='menu'>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Pages</a></li>
+          <li><a href="#">Contact us</a></li>
+          <li><a href="#"><img src={require("./components/Cart.png") }alt="" /></a></li>
+          <li><button className='order'>Order Today</button></li>
+        </ul>
+        <div className="content">
+        <div className="image">
+          <img src={ require("./components/Book.png")} alt="" />
+        </div>
+        <div className="description">
+            <div className='line'>Your Books From</div>
+        </div>
+        </div>
     </div>
   );
 }
